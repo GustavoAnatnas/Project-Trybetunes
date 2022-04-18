43a04be02b7f9c856bfe0506d4288346823eb157
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-18 15:43:06 -0300

    Desafio 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes.</p>);
+    return (
+      <div>
+        <p>TrybeTunes.</p>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
