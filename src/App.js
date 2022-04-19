2d002abff655f388a224e58be47fdb7cb2feda89
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-19 19:46:26 -0300

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Home from './Home';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <p>TrybeTunes.</p>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
+        <h1>Home</h1>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </div>
     );
   }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,80 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      load: false,
+      bttnDisable: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onInputChange(event) {
+    const minNumber = 3;
+    const { value } = event.target;
+    if (value.length >= minNumber) {
+      this.setState({
+        name: value,
+        bttnDisable: false,
+      });
+    }
+  }
+
+  async onClick() {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({
+      load: true,
+    });
+    await createUser({
+      name,
+    });
+    history.push('/search');
+  }
+
   render() {
+    const {
+      load,
+      bttnDisable,
+    } = this.state;
     return (
       <div data-testid="page-login">
-        <p>..</p>
+
+        {
+          load && <Loading />
+        }
+
+        <input
+          name="login"
+          type="text"
+          data-testid="login-name-input"
+          onChange={ this.onInputChange }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ bttnDisable }
+          onClick={ this.onClick }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
