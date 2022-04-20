59f5b1c7ee85aa92f34f50fd3271d00ce75389c1
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-19 22:02:54 -0300

    Desafio 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
-  render() {
-    return (
-      <header data-testid="header-component" />
-    );
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      load: true,
+    };
   }
+
+  componentDidMount() {
+    this.userInfo();
+  }
+
+userInfo = async () => {
+  const user = await getUser();
+
+  this.setState({
+    user: user.name,
+    load: false,
+  });
+}
+
+render() {
+  const {
+    user,
+    load,
+  } = this.state;
+
+  if (load) {
+    return <Loading />;
+  }
+  return (
+    <header data-testid="header-component">
+      <p data-testid="header-user-name">{`Bem Vindo, ${user}`}</p>
+    </header>
+  );
+}
 }
 
 export default Header;
