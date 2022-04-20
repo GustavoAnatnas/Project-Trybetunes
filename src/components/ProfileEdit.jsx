b589c9b1701cfc2145fc649a8a3c2ca953697749
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-19 21:21:44 -0300

    Desafio 3 Import Feito

--- src/components/ProfileEdit.jsx
+++ src/components/ProfileEdit.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 class ProfileEdit extends React.Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
         <p>..</p>
+        <Header />
       </div>
     );
   }
