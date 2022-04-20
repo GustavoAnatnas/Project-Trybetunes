963c80d68134f920b88d7b7b62c2c8b66cc718f2
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-19 23:49:02 -0300

    Desafio 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bttnDisable: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    const minNumber = 2;
+    const { value } = event.target;
+    if (value.length >= minNumber) {
+      this.setState({
+        bttnDisable: false,
+      });
+    }
+  }
+
   render() {
+    const {
+      bttnDisable,
+    } = this.state;
     return (
       <div data-testid="page-search">
-        <p>..</p>
+        <form action="">
+          <input
+            type="text"
+            placeholder="Banda ou Artista"
+            onChange={ this.onInputChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ bttnDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
         <Header />
       </div>
 
