7c3b2c0ec65d6ad9398da2e5a9413f02e97aadc8
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-19 22:13:08 -0300

    Desafio 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -36,7 +37,19 @@ render() {
   return (
     <header data-testid="header-component">
       <p data-testid="header-user-name">{`Bem Vindo, ${user}`}</p>
+      <nav>
+        <Link to="/search" data-testid="link-to-search">
+          Busca
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favoritos
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Perfil
+        </Link>
+      </nav>
     </header>
+
   );
 }
 }
