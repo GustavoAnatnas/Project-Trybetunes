13c8b6f7fa31754032156f368a8b54bafad01662
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-22 00:26:37 -0300

    Atualização Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Loading extends React.Component {
   render() {
-    return <div data-testid="page-loading">Carregando...</div>;
+    return <h1 data-testid="page-loading" className="Load">Carregando...</h1>;
   }
 }
 
